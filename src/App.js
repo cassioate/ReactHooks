60db095e7bbdf840bdbebfda34e4f2399bb13d89
Cassio <cassioate@gmail.com>
2021-05-26 11:26:05 -0300

    Utilizando useMemo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, useMemo} from 'react'
 
 function App() {
 
@@ -15,29 +15,38 @@ function App() {
     }
 
     /** Se utilizado dessa forma, com o array vazio após a função, 
-     *  ele irá executar apenas ao iniciar a aplicação, depois não 
-     *  irá mais fazer nada */
+     * ele irá executar apenas ao iniciar a aplicação, depois não 
+     * irá mais fazer nada */
     useEffect(()=>{
         console.log('Executei no inicio')
     }, [])
 
     /**
      *  Substitui as didmount functions, na inicialização e depois
-     *  toda vez que o techs for alterado ele vai chamar a função 
-     *  que eu passar dentro do useEffect como primeiro parametro
+     * toda vez que o techs for alterado ele vai chamar a função 
+     * que eu passar dentro do useEffect como primeiro parametro
      */
     useEffect(() => {
         localStorage.setItem('techs', JSON.stringify(techs))
     }, [techs])
     // OBS: Está salvando a lista no localStorage do browser (inspencionar elemento/Application/LocalStorage/localhost:3000)
 
+    /** useMemo é utilizado para evitar que seja feito o mesmo calculo toda vez que a tela for renderizada,
+     * ele é utilizado para manter um valor salvo até que uma variavel especifcia seja modificada, quando essa variavel
+     * for modificada, então ele irá realizar o calculo novamente
+     */
+    const techSize = useMemo(()=> techs.length, [techs])
+
     return (
         <>
             <ul>
                 {techs.map(tech => 
-                <li key="tech">{tech}
+                <li key={tech}>{tech}
                 </li>)}
             </ul>
+            {/** Use memo está sendo utilizado aqui */}
+            <strong>Você tem {techSize} tecnologias</strong>
+            <br></br>
             <input value={newTech} onChange={e => setNewTech(e.target.value)}></input>
             <button type="button" onClick={handleAdd}>Add</button>
         </>
